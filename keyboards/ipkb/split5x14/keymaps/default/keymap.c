019b44ce1836a5428fd4686cf455be77b1aec23a
proleskovskiy-i <35276635+proleskovskiy-i@users.noreply.github.com>
2020-12-12 14:00:59 +0200

    Update keymap.c

--- keyboards/ipkb/split5x14/keymaps/default/keymap.c
+++ keyboards/ipkb/split5x14/keymaps/default/keymap.c
@@ -1,6 +1,12 @@
 #include QMK_KEYBOARD_H
 
 
+/*      - дефолтный слой с обычной раскладкой
+        - дефолтный слой с доп. раскладкой numpad, arrows, ins...
+        - кастомный слой с универсальной раскладкой в юникоде
+        - кастомный слой с универсальной цифровой раскладкой в юникоде
+*/
+
 // Layer Declarations
 enum {
     _BASE = 0,
@@ -17,17 +23,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * |------+------+------+------+------+------+------|          |------+------+------+------+------+------+------|
      * | Tab  | A[Ф] | S[Ы] | D[В] | F[А] | G[П] | Del  |          |      | H[Р] | J[О] | K[Л] | L[Д] | [Ж]  | [Э]  |
      * |------+------+------+------+------+------+------|          |------+------+------+------+------+------+------|
-     * | Shift| Z[Я] | X[Ч] | C[С] | V[М] | B[И] |Enter |          |Enter | N[Т] | M[Ь] | [Б]  |  [Ю] | [Ё]  | [Ъ]  |
+     * | Shift| Z[Я] | X[Ч] | C[С] | V[М] | B[И] |Enter |          |Enter | N[Т] | M[Ь] |  [Б] |  [Ю] | [Ё]  | [Ъ]  |
      * |------+------+------+------+------+------+------|          |------+------+------+------+------+------+------|
      * | Ctrl |  Win |      |TT(_NUM)| Alt|Space | Ctrl |          | Ctrl |Space |Shift |      |      |      |      |
      * `------------------------------------------------'          `------------------------------------------------'
      */
     [_BASE] = LAYOUT_ortho_5x14(
-    KC_ESC,  KC_NO ,   KC_NO ,  KC_NO ,  KC_NO ,  KC_NO ,  LCTL(KC_1) ,       KC_6 , KC_7 , KC_8 , KC_9 , KC_0 , KC_BSPC, KC_DEL ,
-    KC_TAB,  KC_Q ,    KC_W ,   KC_E ,   KC_R ,   KC_T ,   LCTL(KC_3) ,               KC_Y , KC_U , KC_I , KC_O , KC_P , KC_LBRC, KC_RBRC ,
-    KC_CAPS, KC_A ,    KC_S ,   KC_D ,   KC_F ,   KC_G ,   KC_DEL ,             KC_H , KC_J , KC_K , KC_L , KC_SCLN, KC_QUOT, KC_ENT ,
-    KC_LSFT, KC_Z ,    KC_X ,   KC_C ,   KC_V ,   KC_B ,   KC_ENT ,             KC_N , KC_M , KC_COMM, KC_DOT , KC_SLSH, KC_HOME, KC_END ,
-    KC_LCTL, KC_LGUI,  KC_NO, TT(_NUM), KC_LALT, KC_SPC ,    KC_LCTL ,            KC_LCTL , KC_LALT , KC_LEFT, KC_DOWN, KC_UP , KC_RGHT, KC_BSLS
+    KC_ESC,  KC_NO,   KC_NO,   KC_NO,    KC_NO,    KC_NO,  LCTL(KC_1),        KC_0 , KC_6 ,    KC_7 ,    KC_8 ,   KC_9 ,   KC_0 ,    KC_BSPC,
+    KC_TAB,  KC_Q,    KC_W,    KC_E,     KC_R,     KC_T,   LCTL(KC_3),        KC_0 , KC_Y ,    KC_U ,    KC_I ,   KC_O ,   KC_P ,    KC_LBRC,
+    KC_CAPS, KC_A,    KC_S,    KC_D,     KC_F,     KC_G,   KC_DEL,            KC_0 , KC_H ,    KC_J ,    KC_K ,   KC_L ,   KC_SCLN,  KC_QUOT,
+    KC_LSFT, KC_Z,    KC_X,    KC_C,     KC_V,     KC_B,   KC_ENT,            KC_0 , KC_N ,    KC_M ,    KC_COMM, KC_DOT , KC_SLSH,  KC_HOME,
+    KC_LCTL, KC_LGUI, KC_NO,   TT(_NUM), KC_LALT,  KC_SPC, KC_LCTL,           KC_0 , KC_LCTL , KC_LALT , KC_LEFT, KC_DOWN, KC_UP ,   KC_RGHT
     ),
         /*
      * ,------------------------------------------------.          ,------------------------------------------------.
